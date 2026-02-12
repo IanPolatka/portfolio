57d366f868b72a2c8e994c383d066dfd221756c8
Ian Polatka <ianpolatka@gmail.com>
2026-02-12 14:19:07 -0500

    Initial Commit

---
+++ src/lib/spotify.ts
@@ -0,0 +1,33 @@
+const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+const RECENTLY_PLAYED_ENDPOINT =
+  "https://api.spotify.com/v1/me/player/recently-played?limit=10";
+
+export async function getRecentlyPlayed() {
+  const basicAuth = Buffer.from(
+    `${import.meta.env.SPOTIFY_CLIENT_ID}:${import.meta.env.SPOTIFY_CLIENT_SECRET}`,
+  ).toString("base64");
+
+  // Get access token
+  const tokenRes = await fetch(TOKEN_ENDPOINT, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basicAuth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: import.meta.env.SPOTIFY_REFRESH_TOKEN,
+    }),
+  });
+
+  const { access_token } = await tokenRes.json();
+
+  // Fetch recently played
+  const res = await fetch(RECENTLY_PLAYED_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  return res.json();
+}
