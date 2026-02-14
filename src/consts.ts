2eed9a3f15b4b17126d51c4317f2657651e126f1
Ian Polatka <ianpolatka@gmail.com>
2026-02-14 11:59:14 -0500

    Added the social media graphic

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Ian Polatka";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "I’m a UI/UX Designer from the Commonwealth of Kentucky. I help businesses figure out the best ways to improve their websites, so customers are happier and their needs are met.";
