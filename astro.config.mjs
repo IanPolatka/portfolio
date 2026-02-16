57a70f7c7a5dd0eb647ab946215f7092b0332aee
Ian Polatka <ianpolatka@gmail.com>
2026-02-15 21:31:10 -0500

    Changed site

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://ianpolatka.com",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
